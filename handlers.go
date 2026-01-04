4be2ee484edff2e3e854276bd85d8d7f0ff20962
Aleksa <aleksa8radovic@gmail.com>
2026-01-04 09:56:11 +0100

    added readiness, metrics, reset handlers and middleware that Incs metrics

---
+++ handlers.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func readinessHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+
+	hits := cfg.fileserverHits.Load()
+	w.Write([]byte(fmt.Sprintf("Hits: %d", hits)))
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
--- main.go
+++ main.go
@@ -3,22 +3,34 @@ package main
 import (
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
 func main() {
-
+	cfg := &apiConfig{}
 	mux := http.NewServeMux()
+	mux.HandleFunc("/healthz", readinessHandler)
+	mux.HandleFunc("/metrics", cfg.metricsHandler)
+	mux.HandleFunc("/reset", cfg.resetHandler)
 
+	fileServer := http.FileServer(http.Dir("."))
+
+	mux.Handle(
+		"/app/",
+		cfg.middlewareMetricsInc(
+			http.StripPrefix("/app", fileServer),
+		),
+	)
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
 
-	err := server.ListenAndServe()
-	if err != nil && err != http.ErrServerClosed {
-		log.Fatal(err)
-	}
-
 	log.Println("Server running on http://localhost:8080")
+	log.Fatal(server.ListenAndServe())
 
 }
