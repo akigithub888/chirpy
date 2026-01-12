56684bbfca578b10dd6fd534623f5d9c34a5f93d
Aleksa <aleksa8radovic@gmail.com>
2026-01-12 12:45:36 +0100

    added database psql opened connetin to database, put it in config (cfg)

--- main.go
+++ main.go
@@ -1,17 +1,36 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
+
+	"github.com/akigithub888/chirpy/internal/database"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	db             *database.Queries
 }
 
 func main() {
-	cfg := &apiConfig{}
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal(".env not loaded")
+	}
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("Unable to open a connection to database")
+	}
+	dbQueries := database.New(db)
+	cfg := &apiConfig{
+		db: dbQueries,
+	}
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /api/healthz", readinessHandler)
 	mux.HandleFunc("GET /admin/metrics", cfg.metricsHandler)
